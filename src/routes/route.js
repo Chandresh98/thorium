43e0d861568bf1bac0f8cc611ee2440872d2731d
Chandresh Shakya <shakyachandresh3@gmail.com>
2022-02-23 00:39:28 +0530

    all completed with 100%

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,63 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
+router.get('/students/:name', function (req, res) {
     let studentName = req.params.name
     console.log(studentName)
     res.send(studentName)
 })
+router.get('/movies', function (req, res) {
+    const movies = ['puspa', 'eternals', 'free guy', 'baby boss', 'harry potter']
+    res.send(movies)
+})
+router.get('/movies/:index', function (req, res) {
+    const index = req.params.index
+    const movies = ['puspa', 'eternals', 'free guy', 'baby boss', 'harry potter']
+    if (index > movies.length) {
+        res.send("use a valid index")
 
+    } else {
+        res.send(movies[index - 1])
+    }
+})
+router.get('/films', function (req, res) {
+    const film = [{
+        id: 1,
+        name: "The Shining"
+    }, {
+        id: 2,
+        name: "Incendies"
+    }, {
+        id: 3,
+        name: "Rang de Basanti"
+    }, {
+        id: 4,
+        name: "Finding Demo"
+    }]
+    res.send(film)
 
-
+})
+router.get('/films/:filmId',function(req,res){
+    const filmId = req.params.filmId
+    const film = [{
+        id: 1,
+        name: "The Shining"
+    }, {
+        id: 2,
+        name: "Incendies"
+    }, {
+        id: 3,
+        name: "Rang de Basanti"
+    }, {
+        id: 4,
+        name: "Finding Demo"
+    }]
+    if(filmId>film.length){
+        res.send("No movie exists with this id")
+    }else{
+        res.send(film[filmId-1])
+    }
+    
+})
 
 module.exports = router;
