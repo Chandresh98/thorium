6bd858fbec7e74ca0c4efca109d36146ae7377b7
Chandresh Shakya <shakyachandresh3@gmail.com>
2022-02-26 23:13:05 +0530

    completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+const bookSchema= require("../models/userModel.js")
+// const UserController= require("../controllers/userController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+// router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+// router.get("/getUsersData", UserController.getUsersData)
+
+router.post("/bookSchema",async function (req, res) {
+    let data = req.body
+    let savedData= await bookSchema.create(data)
+    res.send({msg: savedData})
+})
+
+router.get("/bookSchemaList", async function (req, res) {
+    let  allBook= await bookSchema.find()
+    res.send({msg: allBook})
+})
 
 module.exports = router;
