cbdc047e2743cad7e174c51db9de6f84e5c9f51c
Chandresh Shakya <shakyachandresh3@gmail.com>
2022-03-03 00:33:16 +0530

    populate

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,19 +3,31 @@ const router = express.Router();
 
 const authorController= require("../controllers/authorController")
 const bookController= require("../controllers/bookController")
+const publisherController= require("../controllers/publisherController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
+// . Write a POST api that creates an author from the details in request body
 router.post("/createUser", authorController.createAuthor  )
 
-router.get("/getAuthorsData", authorController.getAuthorsData)
 
-router.post("/createBook", bookController.createBook  )
+// . Write a POST api that creates a publisher from the details in the request body
+router.post("/createpublisher", publisherController.createPublisher)
 
-router.get("/getBooksData", bookController.getBooksData)
+// Write a POST api that creates a book from the details in the request body.
+//  The api takes both the author and publisher from the request body. 
+router.get("/createBook", bookController.createBook )
 
-router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
+// Write a GET api that fetches all the books along with their author details (you have to populate for this) 
+// as well the publisher details (you have to populate for this)
+
+router.get("/getBooks", bookController.getBooks)
+
+
+// router.get("/getBooksData", bookController.getBooksData)
+
+// router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
 
 module.exports = router;
